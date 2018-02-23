2312be8cb27026475f10dc275b2a72534fcf719c
omarichardson01 <omar.richardson01@gmail.com>
2018-02-23 16:57:56 -0500

    Added Navigation scroll effect

--- UdemyCourse/resources/js/script.js
+++ UdemyCourse/resources/js/script.js
@@ -1,3 +1,5 @@
+/* Sticky Navigation */
+
 $(document).ready(function(){
 
     $('.js--section-features').waypoint(function(direction) {
@@ -12,9 +14,33 @@ $(document).ready(function(){
       })   
 });
 
-/*
-var waypoints = $('#handler-first').waypoint(function(direction) {
-    notify(this.element.id + ' hit 25% from top of window') 
-  }, {
-    offset: '25%'
-  }) */
+/*Scroll on buttons */
+
+$('.js--scroll-to-plan').click(function(){
+    $('html,body').animate
+    ({scrollTop: $('.js--section-plans').offset().top
+    },1000);
+});
+
+$('.js--scroll-to-start').click(function(){
+    $('html,body').animate
+    ({scrollTop: $('.js--section-features').offset().top
+    },1000);
+});
+
+/* Navigation Scroll */
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
